c135e1e62ab8bccf21c65c219ac103817da3107f
jiwoo park <jiwoop.jj@gmail.com>
2018-08-12 11:36:34 +0900

    Fix Backtesting date axis based on price file's date.

--- utils.py
+++ utils.py
@@ -9,10 +9,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def make_year_esg_column(from_year=2011, to_year=2018, esg=('E', 'S', 'G')):
-    return list(itertools.chain.from_iterable([list(zip([x] * len(esg), esg))\
-                                                for x in list(range(from_year, to_year))]               \
-                                ))
+    return list(itertools.chain.from_iterable([list(zip([x] * len(esg), esg)) \
+                                               for x in list(range(from_year, to_year))] \
+                                              ))
+
 
 def backtesting(back_obj, price_file, save_file=None, method='eq'):
     """
@@ -21,18 +23,22 @@ def backtesting(back_obj, price_file, save_file=None, method='eq'):
     :param save_file:
     :return:
     """
-    back_obj = OrderedDict(sorted(back_obj.items(), key=lambda v:v))
+    # TODO : date index based on price file
+    back_obj = OrderedDict(sorted(back_obj.items(), key=lambda v: v))
     price_df = pd.read_csv(price_file, header=[0, 1], encoding='euc-kr', parse_dates=[0], index_col=0)
 
     price_val = []
+    date_axis = []
 
-    for key, val in back_obj.items():
-        price_val.extend(price_df[key[0]:key[1]][val].fillna(0).apply(np.average, axis=1))
+    for period, firms in back_obj.items():
+        try:
+            price_val.extend(price_df[period[0]:period[1]][firms].fillna(0).apply(np.average, axis=1))
+            date_axis.extend(price_df[period[0]:period[1]].index.values)
+        except Exception as e:
+            pass
+            # print(key, firms)
+            # print(e)
 
-    start_date = list(back_obj.keys())[0][0]
-    end_date = list(back_obj.keys())[-1][1]
-
-    date_axis = pd.date_range(start_date, end_date, periods=len(price_val))
     plt.plot(date_axis, price_val)
 
     if save_file:
@@ -43,8 +49,7 @@ def backtesting(back_obj, price_file, save_file=None, method='eq'):
 
 if __name__ == "__main__":
     test_obj = {
-        (datetime(2011, 1, 1), datetime(2012, 1, 1)):['A000010', 'A000120', 'A000200'],
-        (datetime(2012, 1, 1), datetime(2013, 1, 1)):['A000110', 'A000220', 'A000300']
+        (datetime(2017, 10, 1), datetime(2018, 1, 1)): ['A000020', 'A000030'],
+        (datetime(2018, 1, 1), datetime(2018, 6, 20)): ['A000020', 'A000030']
     }
     backtesting(test_obj, './data/Adjusted Price.csv', save_file="test.png")
-
